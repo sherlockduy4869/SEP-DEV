2ebd2055dc48a9afd7d729b6d81916d8271e8949
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2023-06-13 18:27:46 +0700

    Feat: adding link to navigate

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./HeaderStyle.css";
 import logo from '../../Assets/Images/logo.png';
 import user from '../../Assets/Images/user.webp';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -10,18 +11,16 @@ const Header = () => {
       </div>
       <div className="header_menu">
         <ul>
-          <li>HOME</li>
-          <li>ABOUT</li>
-          <li>WORK</li>
-          <li>REQUEST</li>
-          <li>CONTACT</li>
+          <li><Link to={"/"}>HOME</Link></li>
+          <li><Link to={"/search-page"}>SEARCH</Link></li>
+          <li><Link to={"/account"}>ACCOUNT</Link></li>
+          <li><Link to={"/about-us"}>ABOUT</Link></li>
         </ul>
       </div>
       <div className="header_icon_user">
         <img src={user} alt="user_icon" />
       </div>
       <div className="page_name">
-        <h1>ABOUT US</h1>
       </div>
     </div>
   );
